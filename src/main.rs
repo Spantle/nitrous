148a915ce76ebe422cba04f2cd5dcb480f0a90df
Spantle <14240384@student.uts.edu.au>
2024-01-10 16:13:56 +1100

    add web app support

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-extern crate env_logger;
 #[macro_use]
 extern crate log;
 
@@ -6,6 +5,19 @@ mod nds;
 mod ui;
 
 fn main() {
+    init();
+
+    let emulator = nds::Emulator::default();
+
+    info!("Initializing UI");
+    let ui_result = ui::init(emulator);
+    if ui_result.is_err() {
+        error!("Error initializing UI: {:?}", ui_result.err().unwrap());
+    }
+}
+
+#[cfg(not(target_arch = "wasm32"))]
+fn init() {
     let is_debug = cfg!(debug_assertions);
     let mut logger = env_logger::Builder::new();
     logger.filter_level(if is_debug {
@@ -15,12 +27,9 @@ fn main() {
     });
     logger.parse_default_env();
     logger.init();
+}
 
-    let emulator = nds::Emulator::default();
-
-    info!("Initializing UI");
-    let ui_result = ui::init(emulator);
-    if ui_result.is_err() {
-        error!("Error initializing UI: {}", ui_result.err().unwrap());
-    }
+#[cfg(target_arch = "wasm32")]
+fn init() {
+    eframe::WebLogger::init(log::LevelFilter::Debug).ok();
 }
--- src/ui/main.rs
+++ src/ui/main.rs
@@ -1,22 +1,34 @@
 use crate::nds::Emulator;
 
+#[cfg(not(target_arch = "wasm32"))]
 pub fn init(emulator: Emulator) -> Result<(), eframe::Error> {
     let options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default().with_min_inner_size(egui::vec2(272.0, 427.0)),
         ..Default::default()
     };
-    eframe::run_native(
-        "Nitrous",
-        options,
-        Box::new(|cc| {
-            let mut fonts = egui::FontDefinitions::default();
-            egui_phosphor::add_to_fonts(&mut fonts, egui_phosphor::Variant::Regular);
+    eframe::run_native("Nitrous", options, Box::new(|cc| creator(cc, emulator)))
+}
 
-            cc.egui_ctx.set_fonts(fonts);
+#[cfg(target_arch = "wasm32")]
+pub fn init(emulator: Emulator) -> Result<(), eframe::Error> {
+    let options = eframe::WebOptions::default();
+    wasm_bindgen_futures::spawn_local(async {
+        eframe::WebRunner::new()
+            .start("nitrous", options, Box::new(|cc| creator(cc, emulator)))
+            .await
+            .expect("Failed to run web app"); // TODO: can we pass this error out somehow?
+    });
 
-            Box::<NitrousGUI>::new(NitrousGUI::new(emulator))
-        }),
-    )
+    Ok(())
+}
+
+fn creator(cc: &eframe::CreationContext, emulator: Emulator) -> Box<dyn eframe::App> {
+    let mut fonts = egui::FontDefinitions::default();
+    egui_phosphor::add_to_fonts(&mut fonts, egui_phosphor::Variant::Regular);
+
+    cc.egui_ctx.set_fonts(fonts);
+
+    Box::<NitrousGUI>::new(NitrousGUI::new(emulator))
 }
 
 #[derive(Default)]
