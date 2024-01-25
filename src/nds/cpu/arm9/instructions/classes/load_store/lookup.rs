1ece1da4eefbf0c4c1d340506b000a304d66e7f8
Spantle <14240384@student.uts.edu.au>
2024-01-25 20:06:40 +1100

    optimize load/store class

--- src/nds/cpu/arm9/instructions/classes/load_store/lookup.rs
+++ src/nds/cpu/arm9/instructions/classes/load_store/lookup.rs
@@ -15,29 +15,34 @@ pub fn lookup<const INST_SET: u16, const IS_REGISTER: bool>(
     bus: &mut Bus,
 ) -> u32 {
     let inst = LoadStoreInstruction::new::<IS_REGISTER>(&*arm9, o_inst);
+    let post_indexing = INST_SET >> 8 & 1 == 0; // P: technically 0 but we've flipped it since 1 is "offset"/"pre-indexed" addressing
+    let is_add = INST_SET >> 7 & 1 == 1; // U
+    let is_unsigned_byte = INST_SET >> 6 & 1 == 1; // B
+    let w = INST_SET >> 5 & 1 == 1; // W
+    let is_load = INST_SET >> 4 & 1 == 1; // L
 
-    let address = if inst.post_indexing {
+    let address = if post_indexing {
         let address = arm9.eru(inst.first_source_register);
-        if inst.is_add {
+        if is_add {
             arm9.r[inst.first_source_register] += inst.addressing_mode;
         } else {
             arm9.r[inst.first_source_register] -= inst.addressing_mode;
         }
         address
-    } else if inst.is_add {
+    } else if is_add {
         arm9.eru(inst.first_source_register) + inst.addressing_mode
     } else {
         arm9.eru(inst.first_source_register) - inst.addressing_mode
     };
 
-    if inst.w {
+    if w {
         arm9.r[inst.first_source_register] = address;
     };
 
     // there's also some unpredictability if it's "Scaled register pre-indexed" and Rn and Rm are the same
     // i'm sure it's fine
-    if !inst.is_unsigned_byte {
-        if inst.is_load {
+    if !is_unsigned_byte {
+        if is_load {
             return instructions::ldr(inst, address, arm9, bus);
         } else {
             return instructions::str(inst, address, arm9, bus);
@@ -46,7 +51,7 @@ pub fn lookup<const INST_SET: u16, const IS_REGISTER: bool>(
 
     logger::warn(format!(
         "unknown load/store inst {} {} {} {} {}",
-        inst.post_indexing, inst.is_add, inst.is_unsigned_byte, inst.w, inst.is_load
+        post_indexing, is_add, is_unsigned_byte, w, is_load
     ));
     1
 }
--- src/nds/cpu/arm9/instructions/classes/load_store/mod.rs
+++ src/nds/cpu/arm9/instructions/classes/load_store/mod.rs
@@ -7,12 +7,6 @@ pub use lookup::lookup;
 use crate::nds::cpu::arm9::{instructions::models::Instruction, Arm9};
 
 pub struct LoadStoreInstruction {
-    pub post_indexing: bool, // P: technically 0 but we've flipped it since 1 is "offset"/"pre-indexed" addressing
-    pub is_add: bool,        // U
-    pub is_unsigned_byte: bool, // B
-    pub w: bool,             // W
-    pub is_load: bool,       // L
-
     pub first_source_register: u8, // Rn
     pub destination_register: u8,  // Rd
     pub addressing_mode: u32,      // bits[11:0] result
@@ -27,12 +21,6 @@ impl LoadStoreInstruction {
         };
 
         LoadStoreInstruction {
-            post_indexing: inst.get_bit(24),
-            is_add: inst.get_bit(23),
-            is_unsigned_byte: inst.get_bit(22),
-            w: inst.get_bit(21),
-            is_load: inst.get_bit(20),
-
             first_source_register: inst.get_bits(16, 19),
             destination_register: inst.get_bits(12, 15),
             addressing_mode,
