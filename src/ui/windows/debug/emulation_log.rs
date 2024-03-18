c6bfcdfc7e4452047e8dbe7f5a2a0f84f6bbd888
Spantle <14240384@student.uts.edu.au>
2024-03-18 17:37:51 +1100

    make emulation log stick to bottom

--- src/ui/windows/debug/emulation_log.rs
+++ src/ui/windows/debug/emulation_log.rs
@@ -27,47 +27,52 @@ impl NitrousGUI {
                 let log = logger::LOGS.lock().unwrap();
                 let text_style = egui::TextStyle::Monospace;
                 let height = ui.text_style_height(&text_style);
-                egui::ScrollArea::vertical().show_rows(ui, height, log.len(), |ui, row_range| {
-                    ui.make_monospace();
+                egui::ScrollArea::vertical()
+                    .stick_to_bottom(true)
+                    .show_rows(ui, height, log.len(), |ui, row_range| {
+                        ui.make_monospace();
 
-                    for row in row_range {
-                        let line = &log[row];
-                        ui.horizontal(|ui| {
-                            // it's actually vertically centered but egui has these the wrong way around??????????
-                            ui.horizontal_centered(|ui| {
-                                let (color, icon) = match line.kind {
-                                    logger::LogKind::Debug => {
-                                        let color = egui::Color32::LIGHT_BLUE;
-                                        (color, ui.icon(color, egui_phosphor::regular::BUG))
-                                    }
-                                    logger::LogKind::Info => {
-                                        let color = egui::Color32::LIGHT_GREEN;
-                                        (color, ui.icon(color, egui_phosphor::regular::INFO))
-                                    }
-                                    logger::LogKind::Warn => {
-                                        let color = egui::Color32::YELLOW;
-                                        (color, ui.icon(color, egui_phosphor::regular::WARNING))
-                                    }
-                                    logger::LogKind::Error => {
-                                        let color = egui::Color32::LIGHT_RED;
-                                        (color, ui.icon(color, egui_phosphor::regular::X_CIRCLE))
-                                    }
-                                };
+                        for row in row_range {
+                            let line = &log[row];
+                            ui.horizontal(|ui| {
+                                // it's actually vertically centered but egui has these the wrong way around??????????
+                                ui.horizontal_centered(|ui| {
+                                    let (color, icon) = match line.kind {
+                                        logger::LogKind::Debug => {
+                                            let color = egui::Color32::LIGHT_BLUE;
+                                            (color, ui.icon(color, egui_phosphor::regular::BUG))
+                                        }
+                                        logger::LogKind::Info => {
+                                            let color = egui::Color32::LIGHT_GREEN;
+                                            (color, ui.icon(color, egui_phosphor::regular::INFO))
+                                        }
+                                        logger::LogKind::Warn => {
+                                            let color = egui::Color32::YELLOW;
+                                            (color, ui.icon(color, egui_phosphor::regular::WARNING))
+                                        }
+                                        logger::LogKind::Error => {
+                                            let color = egui::Color32::LIGHT_RED;
+                                            (
+                                                color,
+                                                ui.icon(color, egui_phosphor::regular::X_CIRCLE),
+                                            )
+                                        }
+                                    };
 
-                                icon.on_hover_text(format!(
-                                    "({:?}) {}",
-                                    &line.kind, &line.timestamp
-                                ));
+                                    icon.on_hover_text(format!(
+                                        "({:?}) {}",
+                                        &line.kind, &line.timestamp
+                                    ));
 
-                                ui.colored_label(color, format!("[{:?}]", &line.source));
+                                    ui.colored_label(color, format!("[{:?}]", &line.source));
 
-                                ui.colored_label(color, &line.content);
+                                    ui.colored_label(color, &line.content);
+                                });
                             });
-                        });
 
-                        ui.separator();
-                    }
-                })
+                            ui.separator();
+                        }
+                    })
             });
     }
 }
