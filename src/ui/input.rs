944dcdbf76d138b8e9c9cb01ba67b75ca4594f8d
Spantle <14240384@student.uts.edu.au>
2024-04-07 18:27:31 +1000

    fixed input emulation

--- src/ui/input.rs
+++ src/ui/input.rs
@@ -6,10 +6,10 @@ impl NitrousGUI {
     pub fn handle_input(&mut self, ctx: &egui::Context) {
         let keyinput = &mut self.emulator.bus.keyinput;
         ctx.input(|i| {
-            keyinput.set_button_select(i.modifiers.shift);
-            keyinput.set_button_start(i.key_down(Key::Enter));
-            keyinput.set_button_up(i.key_down(Key::ArrowUp));
-            keyinput.set_button_down(i.key_down(Key::ArrowDown));
+            keyinput.set_button_select(!i.modifiers.shift);
+            keyinput.set_button_start(!i.key_down(Key::Enter));
+            keyinput.set_button_up(!i.key_down(Key::ArrowUp));
+            keyinput.set_button_down(!i.key_down(Key::ArrowDown));
         });
     }
 }
