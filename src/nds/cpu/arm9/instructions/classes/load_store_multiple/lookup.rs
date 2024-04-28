1eb3455b03c3c049be3172441380eff41ffebb28
Spantle <14240384@student.uts.edu.au>
2024-04-28 18:07:41 +1000

    clean up load/store multiple lookup

--- src/nds/cpu/arm9/instructions/classes/load_store_multiple/lookup.rs
+++ src/nds/cpu/arm9/instructions/classes/load_store_multiple/lookup.rs
@@ -1,7 +1,4 @@
-use crate::nds::{
-    cpu::arm9::models::{Bits, Context, ContextTrait, DisassemblyTrait, Instruction},
-    logger::LoggerTrait,
-};
+use crate::nds::cpu::arm9::models::{Bits, Context, ContextTrait, DisassemblyTrait, Instruction};
 
 use super::{instructions, LoadStoreMultipleInstruction};
 
@@ -16,7 +13,6 @@ pub fn lookup<Ctx: ContextTrait>(inst_set: u16, ctx: &mut Context<Instruction, C
     };
 
     let s = inst_set >> 2 & 1 == 1; // S
-    let is_incremented = inst_set >> 1 & 1 == 1; // W
     let is_load = inst_set & 1 == 1; // L
     let has_15 = ctx.inst.register_list.get_bit(15);
 
@@ -27,28 +23,22 @@ pub fn lookup<Ctx: ContextTrait>(inst_set: u16, ctx: &mut Context<Instruction, C
             ctx.dis.push_str_end_arg("^", None);
 
             if has_15 {
-                return instructions::ldm_3(ctx);
-            } else if !is_incremented {
-                return instructions::ldm_2(ctx);
+                instructions::ldm_3(ctx)
+            } else {
+                // assumes W bit is 0
+                instructions::ldm_2(ctx)
             }
         } else {
-            return instructions::ldm_1(ctx);
+            instructions::ldm_1(ctx)
         }
     } else {
         ctx.dis.set_inst("STM");
 
         if s {
-            if !is_incremented {
-                return instructions::stm_2(ctx);
-            }
+            // assumes W bit is 0
+            instructions::stm_2(ctx)
         } else {
-            return instructions::stm_1(ctx);
+            instructions::stm_1(ctx)
         }
     }
-
-    ctx.logger.log_warn(format!(
-        "unknown load/store multiple inst {} {} {} {}",
-        s, is_incremented, is_load, has_15
-    ));
-    1
 }
