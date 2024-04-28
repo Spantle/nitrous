60c114249d81922a03fecfb9e369b9162f0e6659
Spantle <14240384@student.uts.edu.au>
2024-04-28 18:07:51 +1000

    clean up instruction model

--- src/nds/cpu/arm9/models/instruction.rs
+++ src/nds/cpu/arm9/models/instruction.rs
@@ -1,5 +1,7 @@
 use std::fmt;
 
+use super::Bits;
+
 #[derive(Debug)]
 pub struct Instruction(u32);
 
@@ -16,23 +18,19 @@ impl std::fmt::Binary for Instruction {
 }
 
 impl Instruction {
-    pub fn bits(&self) -> u32 {
-        self.0
-    }
-
     pub fn get_bit(&self, offset: u32) -> bool {
-        (self.0 >> offset) & 1 == 1
+        self.0.get_bit(offset)
     }
 
-    pub fn get_word(&self, offset: u32, to: u32) -> u32 {
-        (self.0 >> offset) & ((1 << (to - offset + 1)) - 1)
+    pub fn get_byte(&self, offset: u32, to: u32) -> u8 {
+        self.get_word(offset, to) as u8
     }
 
     pub fn get_halfword(&self, offset: u32, to: u32) -> u16 {
         self.get_word(offset, to) as u16
     }
 
-    pub fn get_byte(&self, offset: u32, to: u32) -> u8 {
-        self.get_word(offset, to) as u8
+    pub fn get_word(&self, offset: u32, to: u32) -> u32 {
+        (self.0 >> offset) & ((1 << (to - offset + 1)) - 1)
     }
 }
