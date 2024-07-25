9c08618a119db572fe2bc6d991ff2af81fb822f0
Spantle <14240384@student.uts.edu.au>
2024-07-25 23:32:24 +1000

    add the rest of the GBA buttons

--- src/nds/shared/models/keyinput.rs
+++ src/nds/shared/models/keyinput.rs
@@ -18,13 +18,35 @@ impl From<u16> for KEYINPUT {
 }
 
 impl KEYINPUT {
+    const BUTTON_A_OFFSET: u16 = 0;
+    const BUTTON_B_OFFSET: u16 = 1;
     const BUTTON_SELECT_OFFSET: u16 = 2;
     const BUTTON_START_OFFSET: u16 = 3;
+    const BUTTON_RIGHT_OFFSET: u16 = 4;
+    const BUTTON_LEFT_OFFSET: u16 = 5;
     const BUTTON_UP_OFFSET: u16 = 6;
     const BUTTON_DOWN_OFFSET: u16 = 7;
+    const BUTTON_R_OFFSET: u16 = 8;
+    const BUTTON_L_OFFSET: u16 = 9;
 
     pub fn value(&self) -> u16 {
         self.0
+    }
+
+    pub fn get_button_a(&self) -> bool {
+        self.0.get_bit(Self::BUTTON_A_OFFSET)
+    }
+
+    pub fn set_button_a(&mut self, released: bool) {
+        self.0.set_bit(Self::BUTTON_A_OFFSET, released);
+    }
+
+    pub fn get_button_b(&self) -> bool {
+        self.0.get_bit(Self::BUTTON_B_OFFSET)
+    }
+
+    pub fn set_button_b(&mut self, released: bool) {
+        self.0.set_bit(Self::BUTTON_B_OFFSET, released);
     }
 
     pub fn get_button_select(&self) -> bool {
@@ -43,6 +65,22 @@ impl KEYINPUT {
         self.0.set_bit(Self::BUTTON_START_OFFSET, released);
     }
 
+    pub fn get_button_right(&self) -> bool {
+        self.0.get_bit(Self::BUTTON_RIGHT_OFFSET)
+    }
+
+    pub fn set_button_right(&mut self, released: bool) {
+        self.0.set_bit(Self::BUTTON_RIGHT_OFFSET, released);
+    }
+
+    pub fn get_button_left(&self) -> bool {
+        self.0.get_bit(Self::BUTTON_LEFT_OFFSET)
+    }
+
+    pub fn set_button_left(&mut self, released: bool) {
+        self.0.set_bit(Self::BUTTON_LEFT_OFFSET, released);
+    }
+
     pub fn get_button_up(&self) -> bool {
         self.0.get_bit(Self::BUTTON_UP_OFFSET)
     }
@@ -57,5 +95,21 @@ impl KEYINPUT {
 
     pub fn set_button_down(&mut self, released: bool) {
         self.0.set_bit(Self::BUTTON_DOWN_OFFSET, released);
+    }
+
+    pub fn get_button_r(&self) -> bool {
+        self.0.get_bit(Self::BUTTON_R_OFFSET)
+    }
+
+    pub fn set_button_r(&mut self, released: bool) {
+        self.0.set_bit(Self::BUTTON_R_OFFSET, released);
+    }
+
+    pub fn get_button_l(&self) -> bool {
+        self.0.get_bit(Self::BUTTON_L_OFFSET)
+    }
+
+    pub fn set_button_l(&mut self, released: bool) {
+        self.0.set_bit(Self::BUTTON_L_OFFSET, released);
     }
 }
--- src/ui/input.rs
+++ src/ui/input.rs
@@ -6,10 +6,16 @@ impl NitrousGUI {
     pub fn handle_input(&mut self, ctx: &egui::Context) {
         let keyinput = &mut self.emulator.shared.keyinput;
         ctx.input(|i| {
+            keyinput.set_button_a(!i.key_down(Key::X));
+            keyinput.set_button_b(!i.key_down(Key::Z));
             keyinput.set_button_select(!i.modifiers.shift);
             keyinput.set_button_start(!i.key_down(Key::Enter));
+            keyinput.set_button_right(!i.key_down(Key::ArrowRight));
+            keyinput.set_button_left(!i.key_down(Key::ArrowLeft));
             keyinput.set_button_up(!i.key_down(Key::ArrowUp));
             keyinput.set_button_down(!i.key_down(Key::ArrowDown));
+            keyinput.set_button_r(!i.key_down(Key::C));
+            keyinput.set_button_l(!i.key_down(Key::D));
         });
     }
 }
