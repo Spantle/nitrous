a51dfbeaa06270f67de26ec2e42f9a3a5319ecb7
Spantle <14240384@student.uts.edu.au>
2024-07-25 22:46:29 +1000

    create RSB instruction

--- src/nds/cpu/arm/instructions/classes/data_processing/lookup.rs
+++ src/nds/cpu/arm/instructions/classes/data_processing/lookup.rs
@@ -49,6 +49,12 @@ pub fn lookup<const IS_IMMEDIATE: bool, Ctx: ContextTrait>(
         (0b0010, true) => {
             instructions::sub::<true>(&mut ctx);
         }
+        (0b0011, true) => {
+            instructions::rsb::<true>(&mut ctx);
+        }
+        (0b0011, false) => {
+            instructions::rsb::<false>(&mut ctx);
+        }
         (0b0100, false) => {
             instructions::add::<false>(&mut ctx);
         }
