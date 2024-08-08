531b06d62aaf4c62708197484066615c010580e2
Spantle <14240384@student.uts.edu.au>
2024-08-09 00:05:15 +1000

    minor spelling mistake

--- src/nds/arm/instructions/thumb/classes/data_processing/instructions/cmp_1.rs
+++ src/nds/arm/instructions/thumb/classes/data_processing/instructions/cmp_1.rs
@@ -11,7 +11,7 @@ use crate::nds::{
 pub fn cmp_1(ctx: &mut Context<Instruction, impl ContextTrait>) {
     ctx.dis.set_inst("CMP");
 
-    let rn = ctx.inst.get_byte(0, 2);
+    let rn = ctx.inst.get_byte(8, 10);
     let immed_8 = ctx.inst.get_word(0, 7);
     ctx.dis.push_reg_arg(rn, None);
     ctx.dis.push_word_end_arg(immed_8, None);
