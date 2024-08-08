6d3e49cb3e9801cbe64f5ca2af31a917c56eec6c
Spantle <14240384@student.uts.edu.au>
2024-08-08 14:29:47 +1000

    fix slightly incorrect thumb LDR (3) disassembly

--- src/nds/arm/instructions/thumb/classes/load_store/instructions/ldr_3.rs
+++ src/nds/arm/instructions/thumb/classes/load_store/instructions/ldr_3.rs
@@ -12,7 +12,7 @@ pub fn ldr_3(ctx: &mut Context<Instruction, impl ContextTrait>) -> u32 {
     ctx.dis.set_inst("LDR");
 
     let rd = ctx.inst.get_byte(8, 10);
-    let immed_8 = ctx.inst.get_word(0, 7) * 4;
+    let immed_8 = ctx.inst.get_word(0, 7);
     ctx.dis.push_reg_arg(rd, None);
     ctx.dis.push_str_end_arg("PC", Some("["));
     ctx.dis.push_word_end_arg(immed_8, Some(", "));
@@ -20,7 +20,7 @@ pub fn ldr_3(ctx: &mut Context<Instruction, impl ContextTrait>) -> u32 {
     ctx.dis.push_str_end_arg("", Some("]"));
 
     let pc = ctx.arm.r()[15] + 4;
-    let address = (pc.get_bits(2, 31) << 2) + immed_8;
+    let address = (pc.get_bits(2, 31) << 2) + (immed_8 * 4);
     ctx.arm
         .set_r(rd, ctx.arm.read_word(ctx.bus, ctx.shared, address));
 
