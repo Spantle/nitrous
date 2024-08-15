06b21b9bf108e31bb0496c48792109af32e8c475
Spantle <14240384@student.uts.edu.au>
2024-08-15 16:21:55 +1000

    why lmao

--- src/nds/arm/instructions/thumb/classes/data_processing/instructions/sub_2.rs
+++ src/nds/arm/instructions/thumb/classes/data_processing/instructions/sub_2.rs
@@ -18,14 +18,11 @@ pub fn sub_2(ctx: &mut Context<Instruction, impl ContextTrait>) {
     ctx.dis.push_word_arg(immed_8);
 
     let (result, borrow) = ctx.arm.r()[rd].overflowing_sub(immed_8);
-
     ctx.arm.set_r(rd, result);
-
-    let rd = ctx.arm.r()[rd];
     ctx.arm.cpsr_mut().set_negative(result.get_bit(31));
     ctx.arm.cpsr_mut().set_zero(result == 0);
     ctx.arm.cpsr_mut().set_carry(!borrow);
     ctx.arm
         .cpsr_mut()
-        .set_overflow((rd as i32).overflowing_sub(immed_8 as i32).1);
+        .set_overflow((result as i32).overflowing_sub(immed_8 as i32).1);
 }
