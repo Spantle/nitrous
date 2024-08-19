570e5eceb07857c9a4e3ebf4277bec0b96872b55
Spantle <14240384@student.uts.edu.au>
2024-08-19 19:35:20 +1000

    create thumb STRH (2) instruction

--- src/nds/arm/instructions/thumb/classes/load_store/instructions.rs
+++ src/nds/arm/instructions/thumb/classes/load_store/instructions.rs
@@ -6,6 +6,7 @@ mod ldrh_2;
 mod str_1;
 mod strb_1;
 mod strh_1;
+mod strh_2;
 
 pub use ldr_1::*;
 pub use ldr_3::*;
@@ -15,3 +16,4 @@ pub use ldrh_2::*;
 pub use str_1::*;
 pub use strb_1::*;
 pub use strh_1::*;
+pub use strh_2::*;
