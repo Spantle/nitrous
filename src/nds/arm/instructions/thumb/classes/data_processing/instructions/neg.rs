8b3efe2a910d818b873a3be97770b5f409ab1983
Spantle <14240384@student.uts.edu.au>
2024-08-20 14:56:27 +1000

    fixed a warning

--- src/nds/arm/instructions/thumb/classes/data_processing/instructions/neg.rs
+++ src/nds/arm/instructions/thumb/classes/data_processing/instructions/neg.rs
@@ -18,7 +18,7 @@ pub fn neg(ctx: &mut Context<Instruction, impl ContextTrait>) -> u32 {
     ctx.dis.push_reg_arg(rm, None);
 
     let rm = ctx.arm.r()[rm];
-    let (result, borrow) = (0 as u32).overflowing_sub(rm);
+    let (result, borrow) = 0_u32.overflowing_sub(rm);
     ctx.arm.set_r(rd, result);
     ctx.arm.cpsr_mut().set_negative(result.get_bit(31));
     ctx.arm.cpsr_mut().set_zero(result == 0);
