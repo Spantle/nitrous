56fcaac484102fcb88447e1b21f7cf741cc7d042
Spantle <14240384@student.uts.edu.au>
2024-08-27 00:23:16 +1000

    actually use dispcnt a bit

--- src/nds/gpu/gpu2d.rs
+++ src/nds/gpu/gpu2d.rs
@@ -51,7 +51,9 @@ impl Gpu2d {
         let mut pixels = Vec::with_capacity(256 * 192);
         for y in 0..=191 {
             for x in 0..=255 {
-                let addr = (y * 256 + x) as usize * 2;
+                let vram_block = self.dispcnt.get_vram_block();
+                let addr_offset = (vram_block * 0x20000) as usize;
+                let addr = addr_offset + (y * 256 + x) as usize * 2;
                 let mut bytes = [0; 2];
                 bytes.copy_from_slice(&shared.vram_lcdc_alloc[addr..addr + 2]);
                 let halfword = u16::from_le_bytes(bytes);
