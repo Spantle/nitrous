473f15d50c81e2f3c0eec6d6698acbddf4f00424
Spantle <14240384@student.uts.edu.au>
2024-09-10 01:04:23 +1000

    create thumb BIC instruction

--- src/nds/arm/instructions/thumb/classes/data_processing/lookup.rs
+++ src/nds/arm/instructions/thumb/classes/data_processing/lookup.rs
@@ -99,6 +99,7 @@ pub fn lookup_register(inst_set: u16, ctx: &mut Context<Instruction, impl Contex
         0b1010 => instructions::cmp_2(ctx),
         0b1100 => instructions::orr(ctx),
         0b1101 => instructions::mul(ctx),
+        0b1110 => instructions::bic(ctx),
         0b1111 => instructions::mvn(ctx),
         _ => {
             ctx.logger.log_error(format!(
