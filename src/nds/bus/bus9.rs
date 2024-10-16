2f040cd9e5d65d64fef2a1ee37178254c2e7e717
Spantle <14240384@student.uts.edu.au>
2024-10-16 18:53:59 +1100

    optimize bus a bit

--- src/nds/bus/bus9.rs
+++ src/nds/bus/bus9.rs
@@ -149,14 +149,16 @@ impl BusTrait for Bus9 {
             0x04000000..=0x04000003 => shared.gpus.a.dispcnt = value.into_word().into(),
             0x04001000..=0x04001003 => shared.gpus.b.dispcnt = value.into_word().into(),
             0x04000004..=0x04000005 => shared.gpus.dispstat = value.into_halfword().into(),
-            0x04000008..=0x0400000F => {
-                let addr = addr - 0x04000008;
-                shared.gpus.a.bgxcnt[addr / 2] = value.into_halfword().into();
-            }
-            0x04001008..=0x0400100F => {
-                let addr = addr - 0x04001008;
-                shared.gpus.b.bgxcnt[addr / 2] = value.into_halfword().into();
-            }
+
+            0x04000008..=0x04000009 => shared.gpus.a.bgxcnt[0] = value.into_halfword().into(),
+            0x0400000A..=0x0400000B => shared.gpus.a.bgxcnt[1] = value.into_halfword().into(),
+            0x0400000C..=0x0400000D => shared.gpus.a.bgxcnt[2] = value.into_halfword().into(),
+            0x0400000E..=0x0400000F => shared.gpus.a.bgxcnt[3] = value.into_halfword().into(),
+
+            0x04001008..=0x04001009 => shared.gpus.b.bgxcnt[0] = value.into_halfword().into(),
+            0x0400100A..=0x0400100B => shared.gpus.b.bgxcnt[1] = value.into_halfword().into(),
+            0x0400100C..=0x0400100D => shared.gpus.b.bgxcnt[2] = value.into_halfword().into(),
+            0x0400100E..=0x0400100F => shared.gpus.b.bgxcnt[3] = value.into_halfword().into(),
 
             0x04000100..=0x0400010F => logger::warn(
                 logger::LogSource::Bus9,
